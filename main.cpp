2417538d323a0a3ca0e351b1e5a20633845f36fa
evaneliasyoung <evaneliasyoung@users.noreply.github.com>
2019-03-11 18:03:51 -0500

    :sparkles: Version printer

--- main.cpp
+++ main.cpp
@@ -11,7 +11,23 @@
 #include "pch.h"
 #include "argh.h"
 
+#define CGOGGLES_VERSION_ 0x000100
+
+void outputVersion()
+{
+  std::cout << ((CGOGGLES_VERSION_ & 0xFF0000) >> (4 * 4)) << '.';
+  std::cout << ((CGOGGLES_VERSION_ & 0x00FF00) >> (2 * 4)) << '.';
+  std::cout << ((CGOGGLES_VERSION_ & 0x0000FF) >> (0 * 4)) << std::endl;
+}
+
 int main(int argc, const char *argv[])
 {
+  argh::parser cmdl(argv);
+
+  if (cmdl[{"-v", "--ver", "--version"}])
+  {
+    outputVersion();
+  }
+
   return EXIT_SUCCESS;
 }
