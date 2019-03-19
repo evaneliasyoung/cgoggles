68dbd9b72c55f98b766eb90dc87e765fdd90f49f
evaneliasyoung <evaneliasyoung@users.noreply.github.com>
2019-03-19 17:36:21 -0500

    :zap: ctime

--- pch.h
+++ pch.h
@@ -4,7 +4,7 @@
 *
 *  @author    Evan Elias Young
 *  @date      2019-03-11
-*  @date      2019-03-16
+*  @date      2019-03-19
 *  @copyright Copyright 2019 Evan Elias Young. All rights reserved.
 */
 
@@ -16,6 +16,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cmath>
+#include <ctime>
 
 #include <algorithm>
 #include <exception>
