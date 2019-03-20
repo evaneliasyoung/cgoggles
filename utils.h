a9c29105a8a258ab11ffe63253dd34ecef752371
DocCodes <evanyoung762@gmail.com>
2019-03-19 21:56:45 -0500

    :zap: Unused variable 'hogging' memory

--- utils.h
+++ utils.h
@@ -4,7 +4,7 @@
 *
 *  @author    Evan Elias Young
 *  @date      2019-03-12
-*  @date      2019-03-17
+*  @date      2019-03-19
 *  @copyright Copyright 2019 Evan Elias Young. All rights reserved.
 */
 
@@ -13,7 +13,6 @@
 
 #include "pch.h"
 
-extern std::string request;
 extern std::vector<std::string> requests;
 extern bool jsonExport;
 
